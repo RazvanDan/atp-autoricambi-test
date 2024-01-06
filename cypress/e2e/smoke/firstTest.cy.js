d74f5d4e83a523b184029b4a30093e67edb8c2de
RazvanDan <razvan.dan.94@gmail.com>
2024-01-06 22:12:24 +0200

    project setup

---
+++ cypress/e2e/smoke/firstTest.cy.js
@@ -0,0 +1,9 @@
+import { startFromHomePage } from "../../support/startUtils";
+
+describe("Login Scenario", () => {
+  beforeEach(() => {
+    startFromHomePage();
+  });
+
+  it("Should be able to see something", () => {});
+});
---
+++ cypress/support/startUtils.js
@@ -0,0 +1,4 @@
+export const startFromHomePage = () => {
+  cy.visit("https://www.atp-autoricambi.it");
+  //cy.get('[data-testid="uc-accept-all-button"]').click();
+};
